4b6f189cf44daf5bb11b71964ce963c6f7e0ed59
meza68k <meza24k@gmail.com>
2022-06-30 13:48:33 -0500

    text input now loads from local storage

--- Develop/assets/js/storage.js
+++ Develop/assets/js/storage.js
@@ -4,9 +4,24 @@ Storage.prototype.setObj = function(key, obj) {
 Storage.prototype.getObj = function(key) {
     return JSON.parse(this.getItem(key))
 };
-
+var pasteSauce = function(inputId, tBId){ 
+    var description = localStorage.getObj(inputId)
+    var textBox = document.getElementById(inputId)
+    textBox.value = description
+};
 
 var copyPasteStore = function(inputId,tBId ){
     var event = document.getElementById(inputId).value;
     localStorage.setObj(inputId, event);
+    pasteSauce(inputId)
 };
+
+pasteSauce("9am");
+pasteSauce("10am");
+pasteSauce("11am");
+pasteSauce("12pm");
+pasteSauce("1pm");
+pasteSauce("2pm");
+pasteSauce("3pm");
+pasteSauce("4pm");
+pasteSauce("5pm");
